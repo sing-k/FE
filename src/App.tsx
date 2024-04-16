43897d19f8122d07b51cb40f4a198c4c513ceabf
Ahyoung-Kim <eileel@naver.com>
2024-04-16 18:49:27 +0900

    design : 글로벌 스타일 파일 생성
    프로젝트 전체에서 전역으로 스타일을 관리해야하는 경우를 위한 파일
    기본적으로 웹의 스타일을 제거하기 위한 styled-reset 과 box-sizing 속성을 추가하였음
    개발하다가 전역 스타일이 필요하면 이 파일에 명시할것것

--- src/App.tsx
+++ src/App.tsx
@@ -2,9 +2,12 @@ import { Routes, Route } from "react-router-dom";
 
 import { MainPage } from "./components/pages";
 
+import GlobalStyle from "./styles/GlobalStyle";
+
 function App() {
   return (
     <>
+      <GlobalStyle />
       <Routes>
         <Route path="/" element={<MainPage />} />
       </Routes>
