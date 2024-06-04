9a5075f15ad50e7946e8a4e75712544ca5e121f3
baejb <bjb6478@naver.com>
2024-06-04 23:51:59 +0900

    fix: 앨범 페이지 라우팅 수정

--- src/App.tsx
+++ src/App.tsx
@@ -42,6 +42,7 @@ function App() {
         <Route path="/" element={<MainPage />} />
         <Route path="/signup" element={<SignUpPage />} />
         <Route path="/login" element={<LoginPage />} />
+        <Route path="/album/search" element={<AlbumPage />} />
         <Route path="/album/:id" element={<AlbumDetailPage />} />
         <Route path="/album/*" element={<AlbumPage />} />
       </Routes>
