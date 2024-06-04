95e2ee44deca3e58074e8463f2015d985ea9a0fe
baejb <bjb6478@naver.com>
2024-06-04 20:25:11 +0900

    feat : 로그아웃 구현

--- src/components/atoms/navbar/LogoutBtn.tsx
+++ src/components/atoms/navbar/LogoutBtn.tsx
@@ -1,8 +1,33 @@
 import styled from "styled-components";
 
 import color from "../../../styles/color";
+import { useApi } from "../../../hooks";
+import { getToken, clearTokens } from "../../../utils/auth/tokenStorage";
+
 const LogoutBtn = () => {
-  return <LogoutButton>로그아웃</LogoutButton>;
+  const { data, callApi, statusCode } = useApi();
+  const handleLogout = async () => {
+    const accessToken = getToken("accessToken"); // 액세스 토큰 가져오기
+    const refreshToken = getToken("refreshToken");
+    const headers = {
+      Authorization: `Bearer ${accessToken}`,
+    };
+
+    await callApi(
+      "/api/auth/logout",
+      "post",
+      { accessToken: accessToken, refreshToken: refreshToken },
+      headers,
+    );
+  };
+  if (statusCode === 200) {
+    clearTokens();
+    window.location.reload();
+  } else {
+    console.error("로그아웃 실패:", data);
+  }
+
+  return <LogoutButton onClick={handleLogout}>로그아웃</LogoutButton>;
 };
 
 export default LogoutBtn;
@@ -20,12 +45,13 @@ const LogoutButton = styled.button`
   align-items: center;
   margin-top: 5%;
   background-color: #ffffff5b;
-  box-shadow: inset 1px 1px 1px #76767658;
+  box-shadow: 1px 1px 1px #96969657;
   margin-bottom: 5%;
   white-space: nowrap;
   &:hover {
     background-color: rgb(255, 123, 123);
     color: white;
     border: 0.5px solid white;
+    cursor: pointer;
   }
 `;
