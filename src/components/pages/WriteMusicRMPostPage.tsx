754363fda031482e327c9fd026501ddabffbca30
Ahyoung-Kim <eileel@naver.com>
2024-06-18 17:40:16 +0900

    feat : 음악 추천 게시글 작성 페이지 틀 구성
    - 음악 추천 게시글 작성 페이지 틀 구성
    - 이미지 업로드나 유튜브 영상 업로드 같은 세부 기능은 아직 구현이 되지 않았음

--- src/components/pages/WriteMusicRMPostPage.tsx
+++ src/components/pages/WriteMusicRMPostPage.tsx
@@ -3,15 +3,23 @@ import {
   FieldValues,
   UseFormReturn,
   SubmitHandler,
+  Controller,
 } from "react-hook-form";
 
 import WritePostLayout from "../common/WritePostLayout";
 import PostForm from "../organisms/board/PostForm";
+import SelectBtnForm from "../molecules/board/SelectBtnForm";
+import SelectMusicInfoForm from "../organisms/board/SelectMusicInfoForm";
 
 const WriteMusicRMPostPage = () => {
-  const fieldValues: UseFormReturn = useForm<FieldValues>();
+  const fieldValues: UseFormReturn = useForm<FieldValues>({
+    defaultValues: {
+      genre: "발라드",
+      info: "이미지",
+    },
+  });
 
-  const { handleSubmit } = fieldValues;
+  const { register, handleSubmit, control, watch } = fieldValues;
 
   const onSubmit: SubmitHandler<FieldValues> = (data: FieldValues) => {
     console.log(data);
@@ -23,7 +31,29 @@ const WriteMusicRMPostPage = () => {
       onClickPreview={() => console.log("preview")}
       onClickSubmit={handleSubmit(onSubmit)}
     >
-      <PostForm fieldValues={fieldValues} />
+      <PostForm fieldValues={fieldValues}>
+        <Controller
+          name="genre"
+          control={control}
+          defaultValue="발라드"
+          render={({ field }) => (
+            <SelectBtnForm
+              name="genre"
+              label={"장르"}
+              items={["발라드", "힙합", "팝", "알앤비", "록", "클래식"]}
+              field={field}
+              register={register}
+            />
+          )}
+        />
+
+        <SelectMusicInfoForm
+          register={register}
+          control={control}
+          value={watch("info")}
+          name="info"
+        />
+      </PostForm>
     </WritePostLayout>
   );
 };
