b5a843812e84e94a3832821bd78ba536deb6eca5
baejb <bjb6478@naver.com>
2024-07-10 19:04:35 +0900

    chore : 로컬 서버 프로토콜 http -> https로 변경

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,16 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react-swc'
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react-swc";
 
-// https://vitejs.dev/config/
+import fs from "fs";
+import path from "path";
+
 export default defineConfig({
   plugins: [react()],
-})
+  server: {
+    https: {
+      key: fs.readFileSync(path.resolve(__dirname, "cert/localhost-key.pem")),
+      cert: fs.readFileSync(path.resolve(__dirname, "cert/localhost.pem")),
+    },
+    port: 5173,
+  },
+});
